48c2f6be2977bf6ec23d91a662781c4498fe10ce
TactLabs2 <tactlabs2@TactLabs1s-MacBook-Air.local>
2024-06-04 16:15:18 +0530

    removing unwanted file

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('startButton').addEventListener('click', function() {
+    // Set timer for 1 minute (60 seconds)
+    setTimeout(function() {
+        alert('Time is up!');
+    }, 60000); // 60000 milliseconds = 1 minute
+});
